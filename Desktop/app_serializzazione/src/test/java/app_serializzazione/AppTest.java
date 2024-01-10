bc1fd82de07952d7cbbac7f8ef81a5ee84d45ff0
lai2-24 <lai2-24@itismeucci.com>
2024-01-10 09:28:15 +0100

    Created project

---
+++ Desktop/app_serializzazione/src/test/java/app_serializzazione/AppTest.java
@@ -0,0 +1,20 @@
+package app_serializzazione;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
