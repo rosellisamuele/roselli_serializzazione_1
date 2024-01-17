bf8dad44a11ab35a4ef326ad8dcc30f7a00de15d
lai2-24 <lai2-24@itismeucci.com>
2024-01-17 09:07:07 +0100

    Merged classes

---
+++ serializzazione/src/main/java/serializzazione/AppServer.java
@@ -0,0 +1,50 @@
+package serializzazione;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+
+public class AppServer {
+    public static int port = 6789;
+
+    public static void main(String[] args) throws JsonProcessingException{
+
+        ServerSocket serverSocket;
+        try{
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server aperto sulla porta "+port);
+        }catch(IOException e){
+            System.out.println("Errore nella connessione");
+            return;
+        }
+
+        Server server = new Server();
+        User u = new User();
+
+        server.accept(serverSocket);
+
+        server.send("Connessione effettuata");
+
+        u.setNome(server.receive());
+        u.setCognome(server.receive());
+        u.setEta(Integer.parseInt(server.receive()));
+
+        System.out.println("Ricevuto dati dal client. Procedo ad inviarli in formato XML e JSON ...");
+        
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(u);
+        server.send(xml);
+
+        ObjectMapper jsonMapper = new ObjectMapper();
+        String json = jsonMapper.writeValueAsString(u);
+        server.send(json);
+
+
+    }
+
+    
+}
---
+++ serializzazione/src/main/java/serializzazione/User.java
@@ -0,0 +1,42 @@
+package serializzazione;
+
+public class User {
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    public User(String nome, String cognome, int eta){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    public User(){
+        this.nome = "";
+        this.cognome = "";
+        this.eta = 0;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+    public int getEta() {
+        return eta;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+    @Override
+    public String toString() {
+        return "Nome:"+nome+",Cognome:"+cognome+",Eta:"+eta+";";
+    }
+}
